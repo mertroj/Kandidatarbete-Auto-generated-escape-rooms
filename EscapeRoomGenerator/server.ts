73bf2def54e73a9f27fcbfa2ae994791e7d35ec3
Dennis Christensen <dennis.f.christensen@gmail.com>
2024-02-14 15:03:49 +0100

    added new endpoints

--- EscapeRoomGenerator/server.ts
+++ EscapeRoomGenerator/server.ts
@@ -1,11 +1,31 @@
 import express, { Express, Request, Response } from "express";
+import { EscapeRoom } from "./types";
 
 const app: Express = express();
-const port = 8080;
+const port: number = 8080;
+
+const games: Map<string, EscapeRoom> = new Map();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+app.post('/game/create', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.get('/game/join', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.get('/game/leave', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.post('/game/solve', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
