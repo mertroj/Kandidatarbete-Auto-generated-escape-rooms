caee613f92b634257d063e30c7079cc35c5e1490
mertroj <rojmert1999@gmail.com>
2024-02-28 14:35:44 +0100

    backend done, front end started

---
+++ EscapeRoomGenerator/Anagram.ts
@@ -0,0 +1,77 @@
+class Anagram {
+    private anagramQuestion: string;
+    private anagramAnswers: string[];
+
+    constructor(estimatedTime: number) {
+        let difficulty: string;
+        if (estimatedTime >= 1 && estimatedTime <= 3) {
+            difficulty = "easy";
+        } else if (estimatedTime >= 4 && estimatedTime <= 7) {
+            difficulty = "medium";
+        } else {
+            difficulty = "hard";
+        }
+        this.generateRandomAnagram(difficulty);
+    }
+
+    private async loadJSON(): Promise<any> {
+        const response = await fetch('anagrams_tree.json');
+        return await response.json();
+    }
+
+    private async generateRandomAnagram(difficulty: string): Promise<void> {
+        const difficultyRanges: { [key: string]: [number, number] } = {
+            "easy": [3, 4],
+            "medium": [5, 6],
+            "hard": [7, 8]
+        };
+        const [minLength, maxLength] = difficultyRanges[difficulty];
+
+        const anagramsData = await this.loadJSON();
+
+        const randomLength = Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
+
+        const anagrams = anagramsData[randomLength];
+
+        if (anagrams) {
+            const randomIndex = Math.floor(Math.random() * anagrams.length);
+            const randomAnagram = anagrams[randomIndex];
+
+            this.anagramQuestion = `Random Anagram of length ${randomLength}: ${randomAnagram[0]}`;
+            this.anagramAnswers = randomAnagram.slice(1);
+        } else {
+            throw new Error(`No anagrams found for length ${randomLength}.`);
+        }
+    }
+
+    getQuestion(): string {
+        return this.anagramQuestion;
+    }
+
+    getHint(): string {
+        if (this.anagramAnswers.length > 0) {
+            const firstLetter = this.anagramAnswers[0][0];
+            return `I know the first letter is ${firstLetter.toUpperCase()}, but what's the rest?`;
+        } else {
+            return '';
+        }
+    }
+
+    checkAnswer(userInput: string): boolean {
+        const userInputLowercase = userInput.toLowerCase();
+
+        // Check if there's a semicolon in the correct answer
+        if (this.anagramAnswers[0].includes(';')) {
+            const subAnswers = this.anagramAnswers[0].split(';').map(subAnswer => subAnswer.trim());
+            for (const subAnswer of subAnswers) {
+                if (subAnswer === userInputLowercase) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return this.anagramAnswers[0].toLowerCase() === userInputLowercase;
+        }
+    }
+
+}
