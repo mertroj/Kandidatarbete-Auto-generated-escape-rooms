53cf8e08ee816b17f86cdaaaf93cde9b8ae7acd6
Dennis Christensen <dennis.f.christensen@gmail.com>
2024-02-28 13:35:56 +0100

    fixed backend get route to fetch escape rooms

--- EscapeRoomGenerator/server.ts
+++ EscapeRoomGenerator/server.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import { EscapeRoom } from "./types";
+import  { generateEscapeRoom } from './EscapeRoom'
 import cors from "cors";
 import { MathPuzzleRouter } from "./MathPuzzleRouter";
 
@@ -10,28 +11,19 @@ app.use(express.json());
 app.use(cors());
 app.use('/puzzleService', MathPuzzleRouter);
 
-const games: Map<string, EscapeRoom> = new Map();
-
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+  let players = parseInt(String(req.query.players))
+  let difficulty = parseInt(String(req.query.difficulty))
 
-app.post('/game/create', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  if (Number.isNaN(players)) {
+    res.status(400).send("The player query parameter is missing or invalid")
+  } else if (Number.isNaN(difficulty)) {
+    res.status(400).send("The difficulty query parameter is missing or invalid")
+  } else {
+    let er: EscapeRoom = generateEscapeRoom(players, difficulty);
+    res.send(er);
+  }
 });
-
-app.get('/game/join', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.get('/game/leave', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
-app.post('/game/solve', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
