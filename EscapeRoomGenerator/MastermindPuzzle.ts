6155959626011ab881356469b5c742b71a990f99
Ferddan <freoster@student.chalmers.se>
2024-02-28 15:07:30 +0100

    mastermind generator start

---
+++ EscapeRoomGenerator/MastermindPuzzle.ts
@@ -0,0 +1,35 @@
+function generateNumbers(){ //Generate an array of 3 random numbers
+    let numbers = Number[3];
+    for(let i = 0; i < 3; i++){
+        numbers[i] = Math.floor(Math.random() * 9)
+    }
+    return numbers;
+}
+
+export class MastermindPuzzle {
+    id: number;
+    eTime: number = 6;
+    question: string = 'Figure out the 3 digit combination';
+    private hints: string[] = ['The solution is: ', 
+                            'Green numbers are correct and in correct position, yellow are correct but wrong position', 
+                            'After each guess some numbers change colours, maybe that means something'];
+    solution: Number[];
+    private hintLevel: number = 2;
+    constructor(){
+        this.id = Number(new Date());
+        this.solution = generateNumbers();
+    }
+    getSolution(): string{
+        return this.solution.toString();
+    }
+    getHint(): string{
+        if(this.hintLevel == 0){
+            return this.hints[this.hintLevel] + this.getSolution();
+        }
+        if(this.hintLevel >= 0){
+            this.hintLevel--;
+            return this.hints[this.hintLevel + 1];
+        }
+        return 'No more hints.';
+    }
+}
