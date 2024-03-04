b02f8e57df25cd1bca3e9b824b5db911e838b716
mertroj <rojmert1999@gmail.com>
2024-03-04 16:05:12 +0100

    diverging tree started,

--- EscapeRoomGenerator/Diverging Tree.ts
+++ EscapeRoomGenerator/Diverging Tree.ts
@@ -1,11 +1,7 @@
 
 
 function divergingTree(estimatedTime: number) {
-
-    // if (amountOfPuzzles < 5) {
-    //     console.error("DivergingTree: amountOfPuzzles must be at least 5")
-    // }
-    const MAX_PUZZLE_ROW: number = 5;
+    const MAX_PUZZLE_ROW: number = 4;
     const MIN_PUZZLE_ROW: number = 2;
 
     let puzzleBox: any[] = [];
@@ -20,6 +16,9 @@ function divergingTree(estimatedTime: number) {
     }
     const endPuzzle = generateEndPuzzle(estimatedTime);
 
+    // start loop here
+    // if first row generate 3 puzzles and connect them to endPuzzle
+
 
 }
 
@@ -31,7 +30,8 @@ function generateEndPuzzle(estimatedTime: number): any{
 function generatePuzzle(estimatedTime: number, difficulty: string) {
     const remainingTime: number = estimatedTime;
     const puzzle: any = 1 /* your puzzle generation logic goes here */; // TODO: replace with actual puzzle generation logic
-
+    // should become a puzzleNode object, which contains a puzzle and an array of
+    // incoming connections and another array of outgoing connections
     return {
         remainingTime: remainingTime,
         puzzle: puzzle
