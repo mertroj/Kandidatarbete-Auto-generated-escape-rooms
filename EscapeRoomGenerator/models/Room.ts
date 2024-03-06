85dfbb405d672ebfff61f2ad2a3a6c518b3b46d7
Dennis Christensen <dennis.f.christensen@gmail.com>
2024-03-06 14:18:50 +0100

    fixed room connections

--- EscapeRoomGenerator/models/Room.ts
+++ EscapeRoomGenerator/models/Room.ts
@@ -30,54 +30,48 @@ export class Room {
 }
 const rooms : {[Key: string]: Room} = {};
 
-export function createRooms(nr_of_rooms: number, slots_in_room: number): Room[] {
-    function around(pos: point): point[]  {
-        let [x, y] = pos;
-        return [[x+1,y],[x-1,y],[x,y+1],[x,y-1]];
-    }
+function around(pos: point): point[]  {
+    let [x, y] = pos;
+    return [[x+1,y],[x-1,y],[x,y+1],[x,y-1]];
+}
 
+export function createRooms(nr_of_rooms: number, slots_in_room: number): Room[] {
     let visited = new Set();
-    let possible_locations: [number | null, point][] = [[null, [0,0]]];
+    let possible_locations: point[] = [[0,0]];
     let rooms: Room[] = [];
 
     while (rooms.length < nr_of_rooms) {
         let pos_i = getRandomInt(0, possible_locations.length);
-        let [entrance_i, pos] = possible_locations[pos_i];
-        possible_locations.splice(pos_i, 1);
+        let [pos] = possible_locations.splice(pos_i, 1);
 
         if (visited.has(`${pos[0]},${pos[1]}`)) continue;
 
-        let new_room = new Room(...pos, slots_in_room);
-        let i = rooms.length;
-        rooms.push(new_room);
+        rooms.push(new Room(...pos, slots_in_room));
         visited.add(`${pos[0]},${pos[1]}`);
-        
-        if (entrance_i) {
-            let entrance = rooms[entrance_i-1]
-            if (entrance.x < new_room.x) {
-                entrance.right = new_room.id;
-                new_room.left = entrance.id;
-            }
-            else if (entrance.x > new_room.x) {
-                entrance.left = new_room.id;
-                new_room.right = entrance.id
-            }
-            else if (entrance.y < new_room.y) {
-                entrance.down = new_room.id;
-                new_room.up = entrance.id;
-            }
-            else if (entrance.y > new_room.y) {
-                entrance.up = new_room.id;
-                new_room.down = entrance.id;
-            }
-        }
 
         around(pos).forEach((pos) => {
-            possible_locations.push([i, pos]);
+            possible_locations.push(pos);
         })
     }
+    return connectRooms(rooms);
+}
 
-    return rooms;
+function connectRooms(rooms: Room[]): Room[] {
+    let r: Room | undefined;
+    rooms.forEach((room) => {
+        r = rooms.find((r) => r.x === room.x-1 && r.y === room.y)
+        if (r) room.left = r.id
+
+        r = rooms.find((r) => r.x === room.x+1 && r.y === room.y)
+        if (r) room.right = r.id
+
+        r = rooms.find((r) => r.x === room.x && r.y === room.y-1)
+        if (r) room.up = r.id
+
+        r = rooms.find((r) => r.x === room.x && r.y === room.y+1)
+        if (r) room.down = r.id
+    })
+    return rooms
 }
 
 export function getRoom(roomId: string): Room {
