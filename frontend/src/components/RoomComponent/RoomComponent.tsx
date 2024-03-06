1e2b7b98d2a0759af949f26fbf769c4fe8022fdc
Dennis Christensen <dennis.f.christensen@gmail.com>
2024-03-06 14:19:46 +0100

    added dynamic puzzle rendering

--- frontend/src/components/RoomComponent/RoomComponent.tsx
+++ frontend/src/components/RoomComponent/RoomComponent.tsx
@@ -3,13 +3,27 @@ import { Room } from '../../interfaces';
 import Anagram from "../Puzzles/Anagram";
 import MathPuzzle from "../Puzzles/MathPuzzle";
 import './RoomComponent.css'
+import { useEffect, useState } from 'react';
 
 
 function RoomComponent ({room, addHint}: {room: Room, addHint: Function}) {
+    const [puzzles, setPuzzles] = useState<JSX.Element[]>();
+
+    useEffect(() => {
+        let nodes = room.slots.map((puzzle) => {
+            if (puzzle.puzzle) {
+                return <MathPuzzle addHint={addHint} />
+            } else if (puzzle.anagramQuestion) {
+                return <Anagram addHint={addHint} />
+            } else {
+                return <p>Invalid puzzle</p>
+            }
+        })
+        setPuzzles(nodes)
+    }, [room])
     return (
         <div className='justify-content-center puzzle-grid overflow-y-scroll'>
-            <Anagram addHint={addHint}/>
-            <MathPuzzle addHint={addHint}/>
+            {puzzles}
         </div>
     );
 }
