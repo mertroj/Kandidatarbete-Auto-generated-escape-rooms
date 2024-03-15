908b631b5b0fcea50d5ee8f814bcc0d8f1533af3
Omar <oyounes57@gmail.com>
2024-03-15 20:32:44 +0100

    removed unecessary id attribute

--- EscapeRoomGenerator/models/SlidePuzzle/Piece.ts
+++ EscapeRoomGenerator/models/SlidePuzzle/Piece.ts
@@ -2,12 +2,10 @@ import { Position } from "./Position";
 import { v4 as uuidv4 } from 'uuid';
 
 export class Piece{
-    id: string;
     number: number;
     position: Position;
 
     constructor(number: number, position: Position){
-        this.id = uuidv4();
         this.position = position;
         this.number = number;
     }
